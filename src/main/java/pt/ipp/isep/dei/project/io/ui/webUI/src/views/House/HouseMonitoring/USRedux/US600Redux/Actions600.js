c232e78fd04160fc030cc46a63b5f5330698c1eb
1181716 <1181716@isep.ipp.pt>
2019-06-22 18:20:35 +0100

    #522: House menu: configure house button added to bottom

--- src/main/java/pt/ipp/isep/dei/project/io/ui/webUI/src/views/House/HouseMonitoring/USRedux/US600Redux/Actions600.js
+++ src/main/java/pt/ipp/isep/dei/project/io/ui/webUI/src/views/House/HouseMonitoring/USRedux/US600Redux/Actions600.js
@@ -11,7 +11,7 @@ export function fetchTemp() {
   return dispatch => {
     dispatch(fetchTempStarted()); // antes de fazer o get, coloca o loading a true
     axios
-      .get(`https://localhost:8443/houseMonitoring/currentHouseAreaTemperature`, {
+      .get(`https://localhost:8443/house/currentHouseAreaTemperature`, {
           headers: {
             'Authorization': token,
             "Access-Control-Allow-Credentials": true,
