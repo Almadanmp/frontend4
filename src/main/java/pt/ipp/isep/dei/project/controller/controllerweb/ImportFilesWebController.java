0f172714d5fb398e9801c370d52442ad937af2bd
Daniela Carvalho <1181701@isep.ipp.pt>
2019-06-23 00:05:12 +0100

    REF #538 simplifies methods on importFilesWebController

--- src/main/java/pt/ipp/isep/dei/project/controller/controllerweb/ImportFilesWebController.java
+++ src/main/java/pt/ipp/isep/dei/project/controller/controllerweb/ImportFilesWebController.java
@@ -33,6 +33,12 @@ public class ImportFilesWebController {
     private static final String MILLISECONDS = " millisecond(s).";
     private static final String SUCCESS = "Successfully imported - ";
 
+    private static final String OPERATION_GA = "OPERATION_GA";
+    private static final String OPERATION_GA_READINGS = "OPERATION_GA_READINGS";
+    private static final String OPERATION_HOUSE = "OPERATION_HOUSE";
+    private static final String OPERATION_HOUSE_SENSORS = "OP_HOUSE_SENSORS";
+    private static final String OPERATION_HOUSE_READINGS = "OPERATION_HOUSE_READINGS";
+
     @Autowired
     GeographicAreaRepository geographicAreaRepository;
     @Autowired
@@ -52,6 +58,7 @@ public class ImportFilesWebController {
     @Autowired
     private HouseConfigurationUI houseConfigurationUI;
 
+
     /**
      * Method to import files with Geographic Area and Area Sensors
      *
@@ -59,33 +66,10 @@ public class ImportFilesWebController {
      * @return response: string with information regarding success or failure
      */
     @PostMapping("/importGA")
-    public ResponseEntity<Object> importGAFile(
-            @RequestPart("file") MultipartFile file) {
-        if (file.isEmpty()) {
-            return new ResponseEntity<>(EMPTY_FILE, HttpStatus.OK);
-        }
-
-        String result;
-        String filename;
-        try {
-            Path path = saveUploadedFiles(file);
-            String pathToFile = path.toString();
-            filename = file.getOriginalFilename();
-            long startTime = System.currentTimeMillis();
-            int areas = inputHelperUI.acceptPathJSONorXMLAndReadFile(pathToFile, geographicAreaRepository, sensorTypeRepository, areaTypeRepository);
-            if (areas > 0) {
-                long stopTime = System.currentTimeMillis();
-                result = areas + " Geographic Areas have been successfully imported. \n" + IMPORT_TIME + (stopTime - startTime) + MILLISECONDS;
-            } else {
-                result = "\nNo Geographic Areas were imported.";
-            }
-            Files.delete(path);
-        } catch (IOException e) {
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
-        }
-        return new ResponseEntity<>(SUCCESS +
-                filename + ".\n" + result, new HttpHeaders(), HttpStatus.OK);
+    public ResponseEntity<Object> importGAFile(@RequestPart("file") MultipartFile file) {
+        return dataImportProcessor(file, OPERATION_GA);
     }
+
 
     /**
      * Method to import Area Sensor Readings
@@ -94,91 +78,31 @@ public class ImportFilesWebController {
      * @return response: string with information regarding success or failure
      */
     @PostMapping("/importAreaReadings")
-    public ResponseEntity<Object> importGAReadings(
-            @RequestPart("file") MultipartFile file) {
-        if (file.isEmpty()) {
-            return new ResponseEntity<>(EMPTY_FILE, HttpStatus.OK);
-        }
-
-        String result;
-        String filename;
-        try {
-            Path path = saveUploadedFiles(file);
-            String pathToFile = path.toString();
-            filename = file.getOriginalFilename();
-            result = gaSettingsUI.selectImportGAReadingsMethod(pathToFile);
-            Files.delete(path);
-        } catch (IOException e) {
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
-        }
-        return new ResponseEntity<>(SUCCESS +
-                filename + ".\n" + result, new HttpHeaders(), HttpStatus.OK);
+    public ResponseEntity<Object> importGAReadings(@RequestPart("file") MultipartFile file) {
+        return dataImportProcessor(file, OPERATION_GA_READINGS);
     }
 
     /**
      * Method to import files with House, rooms and Energy Grid Data
      *
-     * @param houseFile file to import
+     * @param file file to import
      * @return response: string with information regarding success or failure
      */
     @PostMapping("/importHouse")
-    public ResponseEntity<Object> importHouseFile(@RequestPart("file") MultipartFile houseFile) {
-        if (houseFile.isEmpty()) {
-            return new ResponseEntity<>(EMPTY_FILE, HttpStatus.OK);
-        }
-
-        String result;
-        String filename;
-        House house = houseRepository.getHouses().get(0);
-        try {
-            Path path = saveUploadedFiles(houseFile);
-            String pathToFile = path.toString();
-            filename = houseFile.getOriginalFilename();
-            long startTime = System.currentTimeMillis();
-            readerController.readJSONAndDefineHouse(house, pathToFile);
-
-            long stopTime = System.currentTimeMillis();
-            result = " \n" + IMPORT_TIME + (stopTime - startTime) + MILLISECONDS;
-
-            Files.delete(path);
-        } catch (IOException e) {
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
-        }
-        return new ResponseEntity<>(SUCCESS +
-                filename + ".\n" + result, new HttpHeaders(), HttpStatus.OK);
+    public ResponseEntity<Object> importHouseFile(@RequestPart("file") MultipartFile file) {
+        return dataImportProcessor(file, OPERATION_HOUSE);
     }
 
 
     /**
      * Method to import a file with House Sensors data
      *
-     * @param houseSensorsFile file to import
+     * @param file file to import
      * @return response: string with information regarding success or failure
      */
     @PostMapping("/importHouseSensors")
-    public ResponseEntity<Object> importHouseSensorsFile(@RequestPart("file") MultipartFile houseSensorsFile) {
-
-        if (houseSensorsFile.isEmpty()) {
-            return new ResponseEntity<>(EMPTY_FILE, HttpStatus.OK);
-        }
-        String result;
-        String filename;
-
-        try {
-            Path path = saveUploadedFiles(houseSensorsFile);
-            String pathToFile = path.toString();
-            filename = houseSensorsFile.getOriginalFilename();
-            long startTime = System.currentTimeMillis();
-            int[] importedSensors = houseConfigurationController.readSensors(pathToFile);
-            long stopTime = System.currentTimeMillis();
-            result = importedSensors[0] + " Sensors successfully imported and " + importedSensors[1] + " rejected.\n" +
-                    IMPORT_TIME + (stopTime - startTime) + MILLISECONDS;
-            Files.delete(path);
-        } catch (IOException ex) {
-            return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
-        }
-        return new ResponseEntity<>(SUCCESS +
-                filename + ".\n" + result, new HttpHeaders(), HttpStatus.OK);
+    public ResponseEntity<Object> importHouseSensorsFile(@RequestPart("file") MultipartFile file) {
+        return dataImportProcessor(file, OPERATION_HOUSE_SENSORS);
     }
 
     /**
@@ -188,33 +112,80 @@ public class ImportFilesWebController {
      * @return response: string with information regarding success or failure
      */
     @PostMapping("/importHouseReadings")
-    public ResponseEntity<Object> importHouseReadings(
-            @RequestPart("file") MultipartFile file) {
-        if (file.isEmpty()) {
-            return new ResponseEntity<>(EMPTY_FILE, HttpStatus.OK);
-        }
-
-        String result;
-        String filename;
-        try {
-            Path path = saveUploadedFiles(file);
-            String pathToFile = path.toString();
-            filename = file.getOriginalFilename();
-            result = houseConfigurationUI.selectImportHouseReadingsMethod(pathToFile);
-            Files.delete(path);
-        } catch (IOException e) {
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
-        }
-        return new ResponseEntity<>(SUCCESS +
-                filename + ".\n" + result, new HttpHeaders(), HttpStatus.OK);
+    public ResponseEntity<Object> importHouseReadings(@RequestPart("file") MultipartFile file) {
+        return dataImportProcessor(file, OPERATION_HOUSE_READINGS);
     }
 
 
+    private ResponseEntity<Object> dataImportProcessor(MultipartFile file, String operation) {
+        String result;
+        long startTime = System.currentTimeMillis();
+
+        if (file.isEmpty()) {
+            return createEmptyFileResponse();
+        }
+
+        try {
+            Path path = saveUploadedFiles(file);
+            switch (operation) {
+                case OPERATION_GA:
+                    result = importGAResult(path, startTime);
+                    break;
+                case OPERATION_GA_READINGS:
+                    result = gaSettingsUI.selectImportGAReadingsMethod(path.toString());
+                    break;
+                case OPERATION_HOUSE:
+                    result = importHouseResult(path, startTime);
+                    break;
+                case OPERATION_HOUSE_SENSORS:
+                    result = importHouseSensorsResult(path, startTime);
+                    break;
+                case OPERATION_HOUSE_READINGS:
+                    result = houseConfigurationUI.selectImportHouseReadingsMethod(path.toString());
+                    break;
+                default:
+                    return createBadRequestResponse(new IllegalArgumentException());
+            }
+            Files.delete(path);
+        } catch (IOException e) {
+            return createBadRequestResponse(e);
+        }
+        return createSuccessResponse(file.getOriginalFilename(), result);
+    }
+
+    private String importGAResult(Path path, long startTime){
+        int areas = inputHelperUI.acceptPathJSONorXMLAndReadFile(path.toString(), geographicAreaRepository, sensorTypeRepository, areaTypeRepository);
+        if (areas > 0) {
+            return areas + " Geographic Areas have been successfully imported. \n" + createIntervalMsg(startTime);
+        } else {
+            return "\nNo Geographic Areas were imported.";
+        }
+    }
+
+
+    private String importHouseResult(Path path, long startTime){
+        House house = houseRepository.getHouses().get(0);
+        readerController.readJSONAndDefineHouse(house, path.toString());
+        return " \n" + createIntervalMsg(startTime);
+    }
+
+    private String importHouseSensorsResult(Path path, long startTime){
+        int[] importedSensors = houseConfigurationController.readSensors(path.toString());
+        return importedSensors[0] + " Sensors successfully imported and " + importedSensors[1] + " rejected.\n" + createIntervalMsg(startTime);
+    }
+
+    private String createIntervalMsg(long startTime) {
+        long stopTime = System.currentTimeMillis();
+        return IMPORT_TIME + (stopTime - startTime) + MILLISECONDS;
+    }
+
     /**
-     * Method to save an imported file
+     * Method to save an uploaded file
+     * Temporarily creates a (temporary) file from an uploaded file and return the path to the new file
      *
-     * @param file imported file to save
+     * @param file the file to save
      * @return path to the saved file
+     * @throws IOException if an exception occurs when saving the file
      */
     private Path saveUploadedFiles(MultipartFile file) throws IOException {
         Path path;
@@ -224,6 +195,20 @@ public class ImportFilesWebController {
         Files.write(path, bytes);
 
         return path;
+    }
+
+    private ResponseEntity<Object> createSuccessResponse(String filename, String result) {
+        return new ResponseEntity<>(SUCCESS + filename + ".\n" + result, new HttpHeaders(), HttpStatus.OK);
+
+    }
+
+    private ResponseEntity <Object> createEmptyFileResponse() {
+        return new ResponseEntity<>(EMPTY_FILE, HttpStatus.OK);
+    }
+
+    private ResponseEntity <Object> createBadRequestResponse(Exception e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+
     }
 
 }
