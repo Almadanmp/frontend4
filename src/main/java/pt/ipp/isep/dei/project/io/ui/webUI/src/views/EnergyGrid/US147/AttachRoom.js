abe7a79487b4e08ff2e357b13735353022f0df56
Teresa Varela Gomes <1181730@isep.ipp.pt>
2019-06-22 21:21:26 +0100

    REF #552
    - US147 front end return message to be the same across application

--- src/main/java/pt/ipp/isep/dei/project/io/ui/webUI/src/views/EnergyGrid/US147/AttachRoom.js
+++ src/main/java/pt/ipp/isep/dei/project/io/ui/webUI/src/views/EnergyGrid/US147/AttachRoom.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import 'react-day-picker/lib/style.css';
-import {Alert, Button} from "reactstrap";
-import {attachRoomGrid} from "./Actions";
-import {connect} from 'react-redux';
-import {detachRoomFromGrid} from "../US149/Actions149";
+import {Alert} from "reactstrap";
 
 class AttachRoom extends React.Component {
 
   constructor(props) {
     super(props);
-  //  this.handleSubmit = this.handleSubmit.bind(this);
     this.state = {
       name: '',
     };
@@ -22,15 +18,16 @@ class AttachRoom extends React.Component {
     if ((room.toString()).indexOf("ERROR") != -1) {
       return (
         <div>
-          <div className="help-block"><Alert color="danger">ERROR: {error}</Alert></div>
+          <div className="help-block"><Alert color="danger">{error}</Alert></div>
 
           </div>
         )}
         else
       {
         return (
-          <><h6><i className="fa fa-check-square-o fa-lg"/> The room has been attached!</h6>
-          </>
+          <div>
+            <div className="help-block"><Alert color="success">The room has been successfully attached.</Alert></div>
+          </div>
         );
       }
 
