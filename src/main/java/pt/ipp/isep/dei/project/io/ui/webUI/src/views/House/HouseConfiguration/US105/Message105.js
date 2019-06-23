19e9e098b83c61662c725f1db34d3dfbe3ced04b
Teresa Varela Gomes <1181730@isep.ipp.pt>
2019-06-23 13:16:54 +0100

    REF #554
    - minor fix in US105

--- src/main/java/pt/ipp/isep/dei/project/io/ui/webUI/src/views/House/HouseConfiguration/US105/Message105.js
+++ src/main/java/pt/ipp/isep/dei/project/io/ui/webUI/src/views/House/HouseConfiguration/US105/Message105.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import 'react-day-picker/lib/style.css';
 import {Alert} from "reactstrap";
-import {attachRoomGrid} from "./Actions";
 
 class Message105 extends React.Component {
 
@@ -16,27 +15,53 @@ class Message105 extends React.Component {
 
   render() {
     const {room, name, floor, width, length, height} = this.props;
-    if (room.toString().indexOf("422") != -1) {
+    if (room.toString().indexOf("422") !== -1) {
       return (
         <div>
           <div className="help-block"><Alert color="danger">Invalid room dimensions. Please fix before continuing.</Alert></div>
         </div>
       )
     }
-    else if ((room.toString()).indexOf("409") != -1) {
+    else if ((room.toString()).indexOf("409") !== -1) {
       return (
         <div>
           <div className="help-block"><Alert color="danger">A Room with that name already exists.</Alert></div>
         </div>
       )
     }
-    else if ((room.toString()).indexOf("400") != -1) {
+    else if ((room.toString()).indexOf("400") !== -1) {
       return (
         <div>
           <div className="help-block"><Alert color="danger">Please complete every field before continuing.</Alert></div>
         </div>
       )
-    } else {
+    }
+    else if (floor === '') {
+      return (
+        <div className="help-block"><Alert color="success">
+          <p>The room was successfully created: </p>
+          <ul>
+            <li>
+              Name: {name}
+            </li>
+            <li>
+              Floor: 0
+            </li>
+            <li>
+              Width: {width} m
+            </li>
+            <li>
+              Length: {length} m
+            </li>
+            <li>
+              Height: {height} m
+            </li>
+          </ul>
+        </Alert>
+        </div>
+      );
+    }
+    else {
       return (
         <div className="help-block"><Alert color="success">
           <p>The room was successfully created: </p>
