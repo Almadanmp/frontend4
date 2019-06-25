1772f59a9d340599ad3f83fcabcedd38b8bae5a5
danie <1181699@isep.ipp.pt>
2019-06-25 10:11:04 +0100

    REF #567: Removes unused  block of code && fully tests getRoomWebDtoInGridLinks method.

--- src/test/java/pt/ipp/isep/dei/project/controller/controllerweb/EnergyGridsWebControllerTest.java
+++ src/test/java/pt/ipp/isep/dei/project/controller/controllerweb/EnergyGridsWebControllerTest.java
@@ -38,7 +38,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -534,6 +533,32 @@ class EnergyGridsWebControllerTest {
 
         Mockito.doReturn(roomDTOMinimals).when(energyGridRoomService).getRoomsDtoWebInGrid("B building");
         Mockito.when(userService.getUsernameFromToken()).thenReturn("ADMIN");
+
+
+        //Act
+        ResponseEntity<Object> actualResult = energyGridsWebController.getRoomsWebDtoInGrid("B building");
+
+        //Assert
+        assertEquals(roomDTOMinimals, actualResult.getBody());
+    }
+
+    @Test
+    void seeIfGetRoomsWebDtoInGridLinksProperlyWork() {
+        //Arrange
+        List<RoomDTOMinimal> roomDTOMinimals = new ArrayList<>();
+        RoomDTOMinimal roomDTOMinimal = new RoomDTOMinimal();
+        roomDTOMinimal.setFloor(3);
+        roomDTOMinimal.setLength(3);
+        roomDTOMinimal.setWidth(3);
+        roomDTOMinimal.setName("B107");
+        roomDTOMinimal.setHeight(3);
+        roomDTOMinimals.add(roomDTOMinimal);
+
+        Link linkDelete = linkTo(methodOn(EnergyGridsWebController.class).detachRoomFromGrid(roomDTOMinimal, "B building")).withRel("1. Detach the room from the grid.");
+        roomDTOMinimal.add(linkDelete);
+
+        Mockito.doReturn(roomDTOMinimals).when(energyGridRoomService).getRoomsDtoWebInGrid("B building");
+        Mockito.when(userService.getUsernameFromToken()).thenReturn("admin");
 
 
         //Act
